16fc60e8bfeb1c5b50b2f632d36a9bb0781227b6
Mykyta Serhiienko <mykyta.serhiienko@globallogic.com>
2022-04-11 23:26:03 +0300

    Part 7, add: default image profile, UpdateAccountForm

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -18,12 +19,14 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError(f'User with this username already exists. Please use another one')
+            raise ValidationError(f'User with this username already exists. '
+                                  f'Please use another one')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError(f'User with this email already exists. Please use another one')
+            raise ValidationError(f'User with this email already exists. '
+                                  f'Please use another one')
 
 
 class LoginForm(FlaskForm):
@@ -33,3 +36,25 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('User with this username already exists. '
+                                      'Please use another one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('User with this email already exists. '
+                                      'Please use another one')
